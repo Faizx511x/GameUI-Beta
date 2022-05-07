b42758835e6ba181783dfad3a3b155111e762461
FaizxDev <75949240+Faizx511x@users.noreply.github.com>
2022-05-07 14:26:49 +0300

    Add files via upload

---
+++ src/GameUI/GameUI.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace GameUI;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+class GameUI extends PluginBase implements Listener {
+
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getLogger()->info("GameUI Enabled");
+	}
+
+	public function onCommand(CommandSender $sender, Command $cmd, String $label, Array $args) : bool {
+
+		switch ($cmd->getName()) {
+			case "gameui":
+				if($sender instanceof Player){
+					$this->openMyForm($sender);
+				} else {
+					$sender->sendMessage("Use this command in-game");
+				}
+		    }
+		return true;    
+	}
+
+	public function openMyForm($player){
+	    $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+	    $form = $api->createSimpleForm(function (Player $player, int $data = null){
+	    	$result = $data;
+	    	if($result === null){
+	    		return true;
+	    	}
+	    	switch($result){
+	    		case 0:
+					
+				break;
+					
+				case 1:
+					
+				break;
+				
+				case 2:
+					
+				break;
+				
+				case 3:
+					
+				break;
+				
+				case 4:
+					
+				break;
+				
+				case 5:
+					
+				break;
+				
+				case 6:
+					
+				break;
+	    	}
+	    });
+	    $form->setTitle("§l§bGamesUI");
+		$form->setContent("Please Select Game");
+		$form->addButton("");
+		$form->addButton("");
+		$form->addButton("");
+		$form->addButton("");
+		$form->addButton("");
+		$form->addButton("§cEXIT");
+		$form->sendToPlayer($player);
+		return $form;
+	}
+}
